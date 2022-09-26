5148409dd31626c7f37f548385298fdd1bc9b3d0
mrroxle <114426297+mrroxle@users.noreply.github.com>
2022-09-26 22:16:19 +0530

    Create bgm.js
    Signed-off-by: mrroxle <114426297+mrroxle@users.noreply.github.com>

---
+++ plugins/bgm.js
@@ -0,0 +1,3 @@
+{
+  "bgm": ["Ayin", "Ayinu", "Bgm", "Bot", "Bye", "Da", "Good night", "Hello", "Hi", "Neymar", "Pm", "Sed", "alive", "assist", "ban", "bgm", "bot", "converting", "fake", "fork", "fuck", "music", "myre", "njan", "number", "oombi", "para", "poda", "raganork", "remove", "reply", "sed", "subscribe", "umma", "xxxtentation"]
+}
