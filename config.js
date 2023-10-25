e0b938c4bc9da7d06a0ebf27b65bf9e1725ef6d6
Adarshdeavo <146413077+Adarshdeavo@users.noreply.github.com>
2023-10-25 10:19:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize');
+6const { Sequelize } = require('sequelize');
 const fs = require('fs');
 
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env', override: true });
@@ -16,7 +16,7 @@ process.env.NODE_OPTIONS = '--max_old_space_size=2560'
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG)
 module.exports = {
 	VERSION: 'v4.4.1', 
-    SESSION_ID: process.env.SESSION_ID || '',
+    SESSION_ID: process.env.SESSION_ID || '10_25_H_E_R_M_I_T_I4PX9P0',
     MODE: (process.env.MODE || 'public').toLowerCase(),
     HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
     SEND_READ: (process.env.READ_COMMAND || false),
@@ -54,6 +54,6 @@ module.exports = {
        DATABASE_URL === './database.db' ? new Sequelize({dialect: 'sqlite', storage: DATABASE_URL, logging: false,}) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: {native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false,}),
        RBG_API_KEY: process.env.REMOVE_BG_API_KEY === undefined ? false : process.env.REMOVE_BG_API_KEY,
        BRAIN_ID: process.env.BRAIN_ID || 'bid=168613&key=EfbnX54Iy9PFIFp3',
-       SUDO: process.env.SUDO || '972528277700,0',
+       SUDO: process.env.SUDO || '917907738112,0',
        DEBUG: DEBUG
 };
