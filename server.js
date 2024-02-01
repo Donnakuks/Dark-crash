47b78dd399479ec1abe1bf18edf905ce53e98617
Ezenwugo Emmanuel Ifechukwu <134739434+Ultar12@users.noreply.github.com>
2024-02-02 00:55:04 +0100

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
-server.listen(PORT, LOCAL_ADDRESS, () => {
-  const address = server.address();
-  console.log('server listening at', address);
-});
